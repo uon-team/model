91ede92d3456c7231072248d7fcf144726866278
Gabriel Roy <gab@uon.io>
2019-03-20 18:44:58 -0400

    Member now take in a map of options instead of just validators

--- src/Member.ts
+++ src/Member.ts
@@ -12,22 +12,53 @@ import { Validator } from './Validate';
  * MemberDecorator interface makes tsc happy
  */
 export interface MemberDecorator {
-    (validators?: Validator[]): PropertyDecorator;
-    new(validators: Validator[]): Member;
+    (options?: MemberOptions): PropertyDecorator;
+    new(options: MemberOptions): Member;
 }
 
 /**
  * The metadata for a model's serializable field
  */
-export interface Member {
+export interface Member extends MemberOptions {
+
+    /**
+     * The field key
+     */
     key: string;
+
+    /**
+     * The field value type
+     */
     type?: Type<any>;
+
+}
+
+export interface MemberOptions {
+
+    /**
+     * An array of validator to call when Validate() is invoked 
+     */
     validators?: Validator[];
+
+    /**
+     * When set to true, Validate() will return an error if the 
+     * field is null or undefined.
+     * Defaults to false.
+     */
+    required?: boolean;
+
+    /**
+     * Coerse the incoming value to the target type during deserialization
+     * @param v 
+     */
+    coerse?<I, T>(v: I): T;
+
+
 }
 
 export const Member: MemberDecorator = MakeUnique(`@uon/model/Member`,
     MakePropertyDecorator("Member",
-        (validators?: Validator[]) => ({ validators }),
+        (options?: MemberOptions) => options,
         null,
         (target: any, meta: Member, key: string) => {
 
