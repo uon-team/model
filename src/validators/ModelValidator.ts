4ea2592aa271252db37e0308d9f264bb72ff3885
Gabriel Roy <gab@uon.io>
2019-09-17 16:59:02 -0400

    Added ModelValidator

---
+++ src/validators/ModelValidator.ts
@@ -0,0 +1,30 @@
+
+import { ValidationFailure, Validator, Validate } from '../Validate';
+import { Type, Injector } from '@uon/core';
+
+/**
+ * Create a model validator
+ * @param modelType 
+ * @param extra 
+ */
+export function ValidateModel<T>(extra?: { [k in keyof Partial<T>]: Validator[] }): Validator {
+
+    const func = async function validateModel(model: any, key: string, val: T, injector?: Injector) {
+
+        let validation = await Validate(val, extra, injector);
+
+        if (!validation.valid) {
+            throw new ValidationFailure(ValidateModel,
+                key,
+                val,
+                validation.failures.map(f => '\t' + f.key + ': ' + f.reason).join('\n')
+            );
+        }
+
+        return val;
+
+    }
+
+    return func;
+
+}
